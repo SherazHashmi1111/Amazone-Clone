50654133cf073fa2f688958c538eefa17c9456bc
sheraz hashmi <sherazhashmi111@gmail.com>
2025-02-06 10:26:17 +0500

    basic enviroment of backend is completed

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+// Route imports
+const products = require("./routes/productRoute");
+
+app.use("/api/v1", products);
+
 module.exports = app;
---
+++ backend/controllers/productController.js
@@ -0,0 +1,6 @@
+exports.getAllProducts = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: 'This route will show all products in database'
+    });
+}
