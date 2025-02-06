7b2cec6a28bf0d28f370e6f7d1aec83a9c43f863
sheraz hashmi <sherazhashmi111@gmail.com>
2025-02-06 11:06:45 +0500

    Create models/productModel/productschema and make a new product and test in postman

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,19 @@
+const Product = require('../models/productModel');
+
+// Create new product => /api/v1/admin/product/new
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    });
+}
+
+// Getting all products => /api/v1/products
 exports.getAllProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
         message: 'This route will show all products in database'
     });
 }
+
