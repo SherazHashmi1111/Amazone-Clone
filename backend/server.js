03a3ecb6099b066abd6a8a3214d6833c16309c75
sheraz hashmi <sherazhashmi111@gmail.com>
2025-02-06 10:41:12 +0500

    Database have been connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-
+const connectDatabase = require("./config/database");
 // Config
-dotenv.config({path: 'backend/config/config.env'})
+dotenv.config({ path: "backend/config/config.env" });
+
+// Database connection
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
