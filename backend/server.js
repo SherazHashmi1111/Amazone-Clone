4b4e3164b16a3dbec7fa59e6b2702f78bedcde8d
sheraz hashmi <sherazhashmi111@gmail.com>
2025-02-09 11:59:11 +0500

    Handling Uncaught error creating error handler func in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
+
+// Handling Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
+});
 // Config
 dotenv.config({ path: "backend/config/config.env" });
 
@@ -10,7 +17,6 @@ connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
-
 
 // unhandled promise rejection
 process.on("unhandledRejection", (err) => {
